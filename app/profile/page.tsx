fccc11aa41e9dcc2e05d1f26b911150b02954361
Sarath <sarathbhushan04@gmail.com>
2026-01-21 15:39:02 +0530

    Fix profile prerender issue by removing revalidate

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+
 export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+
 
 type User = {
   email: string;
